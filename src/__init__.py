235acc7c50e417f6a19da00154c040b9535492cb
Aya <ayatollahibrahim3@gmail.com>
2025-12-18 01:16:14 +0200

    Add files via upload

--- src/__init__.py
+++ src/__init__.py
@@ -1,33 +1,18 @@
-"""
-Egyptian Hieroglyphic Few-Shot Learning Package
+"""Data loading, preprocessing, and augmentation utilities."""
 
-A comprehensive framework for few-shot classification of hieroglyphic glyphs
-using meta-learning approaches including HPGN and ProtoNet.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Aya"
-
-from src.models import HPGN_Small, ProtoNet
-from src.data import (
-    ClassFolderDataset,
+from src.data.preprocessing import ClassFolderDataset, preprocess_image
+from src.data.augmentation import (
     get_train_transform,
     get_val_transform,
-    sample_episode
+    load_image
 )
-from src.training import Trainer, evaluate_on_episodes
-from src.utils import bootstrap_ci, save_json, save_csv
+from src.data.episode_sampler import sample_episode
 
 __all__ = [
-    "HPGN_Small",
-    "ProtoNet",
     "ClassFolderDataset",
+    "preprocess_image",
     "get_train_transform",
     "get_val_transform",
+    "load_image",
     "sample_episode",
-    "Trainer",
-    "evaluate_on_episodes",
-    "bootstrap_ci",
-    "save_json",
-    "save_csv",
 ]
